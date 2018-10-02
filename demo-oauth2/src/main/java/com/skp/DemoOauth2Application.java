0778ed74faeb10f10eab6235b3314666dfdfa25e
jkkim74 <jack68@naver.com>
2018-10-02 17:47:42 +0900

    oauth 추가

---
+++ demo-oauth2/src/main/java/com/skp/DemoOauth2Application.java
@@ -0,0 +1,12 @@
+package com.skp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoOauth2Application.class, args);
+	}
+}
---
+++ demo-oauth2/src/main/java/users/UserService.java
@@ -0,0 +1,66 @@
+package users;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService{
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	@Autowired
+	PasswordEncoder passwordEncoder;
+
+	public List<User> findAll() {
+		// TODO Auto-generated method stub
+		return userRepository.findAll();
+	}
+
+	public User save(User user) {
+		// TODO Auto-generated method stub
+		user.setPassword(passwordEncoder.encode(user.getPassword()));
+		return userRepository.save(user);
+	}
+
+	public void delete(Long id) {
+		// TODO Auto-generated method stub
+		userRepository.deleteById(id);
+	}
+	
+	@PostConstruct
+	public void init() {
+		User jkkim = userRepository.findByUsername("jkkim");
+		if(jkkim == null) {
+			User user = new User();
+			user.setUsername("jkkim");
+			user.setPassword("1234");
+			User newUser = this.save(user);
+			System.out.println(newUser);
+		}
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByUsername(username);
+		return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), getAuthorities());
+	}
+
+	private Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+	}
+
+}
